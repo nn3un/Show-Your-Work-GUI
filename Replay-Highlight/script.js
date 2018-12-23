c180df22535d532b6f3e18aa96db96d1770e9e96
nn3un <nn3un@virginia.edu>
2018-12-22 22:54:30 -0500

    Removed pause button, added reverse and date

--- Replay-Highlight/script.js
+++ Replay-Highlight/script.js
@@ -1,4 +1,6 @@
 
+
+
 //global variables
 var log, content, index, order, paused, data, ordersAtIndices, highlightAtIndices, tokenAtIndices, playButton, pauseButton, fastForwardButton, slider, code, alert, speedDropDown, prevTime;
 
@@ -9,30 +11,35 @@ const msInASec =  1000, msInAMin = 60*1000, msInAHr = 60*60*1000, msInADay = 24*
 //Initialize the different DOM elements
 document.addEventListener("DOMContentLoaded", function(event) { 
     playButton        = document.getElementById("play");
-    pauseButton       = document.getElementById("pause");
     fastForwardButton = document.getElementById("fastForward");
+    reverseButton     = document.getElementById("reverse");
     slider            = document.getElementById("slider");
     alert             = document.getElementById("alert");
     code              = document.querySelector("code");
     speedDropDown     = document.getElementById("speed");
+    date              = document.getElementById("date");
+    steps             = document.getElementById("steps");
 
     window.addEventListener("keydown", function checkKeyPressed(e) {
-        if (e.key == " ") {
+        if (e.key == "n") {
             fastForward();
+        }
+        else if(e.key == "b"){
+            playButton.onmousedown();
+        }
+        else if(e.key == "v"){
+            reverse();
         }
     }, false);
     
 });
 
-function fastForward(){
-    slider.value = parseInt(slider.value) + 10;
-    sliderChanged();
-};
 
 
 //The event listener for the open file
 function openFile() {
     pause();
+    playButton.innerText = "Play"
     var input = event.target;
     var reader = new FileReader();
     reader.onload = function(){
@@ -59,6 +66,7 @@ function openFile() {
     
 };
 
+//https://github.com/tychonievich/archimedes/blob/master/view.php
 var re_comment  = /(#[^\n]*)/;
 var re_string   = /((?:\br?b|\bbr)?"""(?:[^"\\]|\\[\s\S]|""?(?=[^"]))*"""|(?:r?b|br)?'''(?:[^'\\]|\\[\s\S]|''?(?=[^']))*'''|(?:r?b|br)?"(?:[^"\\\n]|\\[\s\S])*"|(?:r?b|br)?'(?:[^'\\\n]|\\[\s\S])*')/;
 var re_number   = /\b((?:[0-9]*\.[0-9]+(?:[eE][-+][0-9]+)?|[0-9]+\.(?:[eE][-+][0-9]+)?|[0-9]+[eE][-+][0-9]+|0[Bb][01]+|0[Oo][0-7]+|0[Xx][0-9a-fA-F]+|0|[1-9][0-9]*)[jJ]?)\b/;
@@ -71,10 +79,13 @@ function htmlspecialchars(s) {
 }
 
 
-async function play(){
+async function resume(){
     paused = false; //Set paused to false
-    pauseButton.disabled = false; //Enable the pause button
-    playButton.disabled = true; //Disable the play button
+    //pauseButton.disabled = false; //Enable the pause button
+    //playButton.disabled = true; //Disable the play button
+    playButton.innerText = "Pause"
+    playButton.onmousedown =  function () { pause(); };
+    playButton.setAttribute("style", "background-color: #4380e0;");
     //The local variables
     var entry, time, type, offset, toChange, htmlToDisplay, currentInd, indAtContent, interval;
     //We're going to start at the current index denoted by the slider, since the user can change slider postion
@@ -84,7 +95,7 @@ async function play(){
     order = 0;
     ordersAtIndices = [];
     tokenAtIndices  = [];
-    //todo: try once more to implement catchUp from old index to new index, instead of starting from 0
+    //TODO: try once more to implement catchUp from old index to new index, instead of starting from 0
     catchUp(0, index);
 
     //Set prevTime to be the time from the last index
@@ -99,6 +110,8 @@ async function play(){
         //Gather the data: time, type, offset and current string
         entry = data[i];
         [time, type, offset, toChange] = [entry[0], entry[1], entry[2], entry[3]];
+        //Display the time
+        date.innerText = (new Date(time)).toString();
         //Update content and the array orderAtIndices with the new entry information
         updateContentAndOrders(i, time, type, offset, toChange);
         updateTokens();
@@ -106,7 +119,6 @@ async function play(){
         [oldHtmlToDisplay, htmlToDisplay] = getHtmlsToDisplay(offset, type, toChange);
         //Find out how much time should be spent showing transitions
         interval = getInterval(time, prevTime);
-        console.log(prevTime);
         //If we've deleted we're going to show the most-recent html with the part being deleted highlighted first
         if(type == "sub") displayHtmlInCode(oldHtmlToDisplay)
         //wait a few ms so that the user can see the transition
@@ -123,8 +135,9 @@ async function play(){
 
 function pause(){
     paused = true;
-    pauseButton.disabled = true;
-    playButton.disabled = false;
+    playButton.innerText = "Resume";
+    playButton.onmousedown = function(){resume();}
+    playButton.setAttribute("style", "background-color:#4CAF50;");
 }
 
 function getInterval(time, prevTime){
@@ -140,11 +153,13 @@ function getInterval(time, prevTime){
     return interval
 }
 
+
 function getHtmlsToDisplay(offset, type, toChange){
     var currentInd    = 0; //currentInd in the order array
     var indAtContent  = 0; //index to track where we are in the code content
     var oldHtmlToDisplay = ""; //Initialize what the output was displaying
     var htmlToDisplay    = "";//Initialize what the output would display
+    reorganizeOrderAtIndices(); 
     //Loop through the entire content
     while(indAtContent < content.length){
         //We want to be able to highlight the portion we're changing so let's check if we're there
@@ -174,7 +189,7 @@ function getHtmlsToDisplay(offset, type, toChange){
         //There might be indices next to each other with the same order, if so it's more efficient to concatanate them together before applying a span style. 
         while(currentInd < ordersAtIndices.length && indAtContent < content.length && ordersAtIndices[currentInd] === currentOrder){
             if(tokenAtIndices[indAtContent]){
-                //This is where syntax highligting is being applied based on the token
+                //This is where syntax highligting is being applied based on the token to every character seperately
                 currentString += '<span class="'+tokenAtIndices[indAtContent]+'">'+content[indAtContent]+'</span>';
             }
             else{
@@ -192,11 +207,62 @@ function getHtmlsToDisplay(offset, type, toChange){
     return [oldHtmlToDisplay, htmlToDisplay]
 }
 
+//Updates ordersAtIndices so that if all contents of a certain order have been deleted, all the other orders are updated 
+function reorganizeOrderAtIndices(){
+    if(ordersAtIndices.length == 0){
+        return;
+    }
+    //First copy the array to sort it
+    var copy = ordersAtIndices.slice();
+    //The set will help keep track of duplicated
+    var set = new Set();
+    //We need to find the number of distinct elements so that we can make future calculations easier
+    var numberOfDistincts = copy.length;
+    set.add(copy[0]);
+    //Using insertion sort, since the array will be mostly sorted,since older things are normally later in the file
+    for(k = 1; k < copy.length; k++){
+        if(set.has(copy[k])){
+            //If we've already seen this, we want to send it to the back, which we can do through setting it to inifinity
+            copy[k] = Infinity;
+            numberOfDistincts--;
+            continue;
+        }
+        set.add(copy[k]);
+        var j = k;
+        while(j > 0 && copy[j-1] > copy[j] ){
+            var temp = copy[j];
+            copy[j] = copy[j-1];
+            copy[j-1] = temp;
+            j -= 1;
+        }
+    }
+    copy = copy.slice(0, numberOfDistincts);
+    order = copy.length;
+    for (k = 0; k < ordersAtIndices.length; k++){
+        var val = ordersAtIndices[k];
+        var start = 0;
+        var end = copy.length - 1;
+        while(true){
+            var mid = parseInt((start + end)/2);
+            if (copy[mid] == val){
+                ordersAtIndices[k] = mid;
+                break;
+            }
+            if(copy[mid] > val){
+                end = mid-1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+    }
+}
+
+
 async function displayHtmlInCode(html){
     var lines = html.split('\n');
     var wid = String(lines.length).length;
     var src = '';
-    //figure out why lieno is not working properly
     for(var j=0; j<lines.length; j+=1) {
         src += '<span class="lineno">'+String(j+1).padStart(wid)+'</span>' + lines[j] + '\n';
     }
@@ -230,7 +296,7 @@ function sleep(ms) {
 
 function updateContentAndOrders(ind, time, type, offset, toChange){
     if (type === "add"){
-        //If it was an add update the content, and for the orderAtIndices array update the order at those specific indices
+        //If it was an add update the content, and for the orderAtIndices array update the order at those specific indices to be the current order
         Array.from(toChange).forEach(function(item, index){
            ordersAtIndices.splice(offset+index, 0, order); 
         })
@@ -243,10 +309,6 @@ function updateContentAndOrders(ind, time, type, offset, toChange){
            ordersAtIndices.splice(offset+index, 1);
         })
         content = content.substring(0, offset) + content.substring(offset + toChange.length); 
-    }
-    else{
-        console.log("eof?")
-        console.log(data[ind])
     }
 }
 
@@ -272,6 +334,16 @@ function updateTokens(){
     }
 }
 
+function fastForward(){
+    slider.value = parseInt(slider.value) + parseInt(parseFloat(steps.value)*data.length) ;
+    sliderChanged();
+};
+
+function reverse(){
+    slider.value = parseInt(slider.value) - parseInt(parseFloat(steps.value)*data.length) ;
+    sliderChanged();
+};
+
 function catchUp(oldIndex, newIndex){
     //This works with fast forward, instead of updating the view, we just update content and the order array. 
     for(i = oldIndex; i < newIndex; i++){
@@ -283,10 +355,10 @@ function catchUp(oldIndex, newIndex){
 
 function sliderChanged(){
     updateSliderAppearance();
-    //when the slider changed, and we are paused we can just re-iterate what happens when a pause and a play happen, since that way play is forced to start from the current slider position
+    //when the slider changed, and we are not paused we can just re-iterate what happens when a pause and a play happen, since that way play is forced to start from the current slider position
     if(!paused){
         pause();
-        play();
+        resume();
     }
     //if we are paused then we will just fast forward to the time indicated by the timer
     else{
@@ -336,12 +408,14 @@ function CSVToArray( strData, strDelimiter ){
         while (arrMatches = objPattern.exec( strData )){
             if (!arrMatches[3]) break;
             var time = parseInt(arrMatches[ 3 ]);
+            if (isNaN(time)) break
             arrMatches = objPattern.exec( strData );
             if (!arrMatches[3]) break;
             var type = arrMatches[3];
             arrMatches = objPattern.exec( strData );
             if (!arrMatches[3]) break;
             var offset = parseInt(arrMatches[3]);
+            if (isNaN(offset)) break
             arrMatches = objPattern.exec(strData);
             if (arrMatches[ 2 ]){
                 var toChange = arrMatches[ 2 ].replace(
@@ -359,7 +433,7 @@ function CSVToArray( strData, strDelimiter ){
 
 // convert a number to a color using hsl
 function numberToColorHsl(i) {
-    var hue = i * 1.2 / 360;
+    var hue = i * 1.8 / 360;
     var rgb = hslToRgb(hue, 1, .8);
     return 'rgba(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ', 0.5)'; 
 }
@@ -404,9 +478,10 @@ function isScrolledIntoView(el) {
 
 /*
 Possible improvements:
-1. Use gap buffer to increase the playback efficiency
+1. Use gap buffer/rope to increase the playback efficiency
 2. Keep track of current minimum order value, so that if a person types everything deletes them and starts again, everything doesn't look green
 3. Cross-browser support??
-4. Add smooth scrolling
+4. Fix scrolling
+5. Figure out why lineno doesn't always work properly
 */
     
